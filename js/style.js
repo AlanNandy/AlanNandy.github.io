961bda742e14143696efadf12ee83a5955eaba55
AlanNandy <2503943844@qq.com>
2017-06-16 10:42:56 +0800

    AlanNandy

--- js/style.js
+++ js/style.js
@@ -1,78 +1,55 @@
-  $(document).ready(function() {
-	  
-	  
-	  
-  /*-------------------------------------------------------------------------------
-    Navigation section
-  -------------------------------------------------------------------------------*/
-	  
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-	  
-    $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-  });
-	  
-	  
-	  
-  /*-------------------------------------------------------------------------------
-    jQuery Parallax
-  -------------------------------------------------------------------------------*/
-	  
-    function initParallax() {
-    $('#home').parallax("100%", 0.1);
-    $('#specialty').parallax("100%", 0.3);
-    $('#hobby').parallax("100%", 0.3);
-    $('footer').parallax("100%", 0.2);
+$(window).load(function() {
+	$('.preloader').fadeOut(1000);
+});
 
-  }
-  initParallax();
+$(document).ready(function() {
 
+	$('.navbar-collapse a').click(function() {
+		$(".navbar-collapse").collapse('hide');
+	});
 
+	$(window).scroll(function() {
+		if($(".navbar").offset().top > 50) {
+			$(".navbar-fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".navbar-fixed-top").removeClass("top-nav-collapse");
+		}
+	});
 
-  /*-------------------------------------------------------------------------------
-    smoothScroll js
-  -------------------------------------------------------------------------------*/
-  
-    $(function() {
-        $('.custom-navbar a, #home a').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 50
-            }, 1000);
-            event.preventDefault();
-        });
-    });
-	  
-	  
-	  
-  /*-------------------------------------------------------------------------------
-    wow js
-  -------------------------------------------------------------------------------*/
+	function initParallax() {
+		$('#home').parallax("100%", 0.1);
+		$('#specialty').parallax("100%", 0.3);
+		$('#hobby').parallax("100%", 0.3);
+		$('footer').parallax("100%", 0.2);
 
-  new WOW({ mobile: false }).init();
-	  
-	  
-	  
-  /*-------------------------------------------------------------------------------
-    wow js
-  -------------------------------------------------------------------------------*/
-	  
-	  $(function(){  
-    $('#GoTop').hide();  
-    $(function(){  
-        $(window).scroll(function(){  
-            if($(window).scrollTop()>50){$('#GoTop').fadeIn(200);}
-            else{$('#GoTop').fadeOut(200);}
-            });
-        });
-    });
-	  
-	  
-	  
-  }); 
+	}
+	initParallax();
+
+	$(function() {
+		$('.custom-navbar a, #home a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top - 49
+			}, 1000);
+			event.preventDefault();
+		});
+	});
+
+	new WOW({
+		mobile: false
+	}).init();
+
+	$(function() {
+		$('#GoTop').hide();
+		$(function() {
+			$(window).scroll(function() {
+				if($(window).scrollTop() > 50) {
+					$('#GoTop').fadeIn(200);
+				} else {
+					$('#GoTop').fadeOut(200);
+				}
+			});
+		});
+	});
+
+});
